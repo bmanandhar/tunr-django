e41a1a7b49fb6d9e42ae93fbd90bf4e42c48e872
Bijaya Manandhar <bijaya.manandhar@yahoo.com>
2018-12-24 14:04:54 -0800

    commit-1

--- tunr/admin.py
+++ tunr/admin.py
@@ -1,5 +1,39 @@
 from django.contrib import admin
 
 # Register your models here.
-# admin.site.register(Artist)
-# admin.site.register(Song)
+
+'''Below this line is new code'''
+# Import the models before executing command
+from tunr.models import Artist
+from tunr.models import Song
+# Select all of the artist objects in the database
+Artist.objects.all()
+
+# Get the artist with the id of 1 (can also do pk here which stands for primary key)
+Artist.objects.get(id=1)
+
+# Get the artist with the name "Sigur Ros", if there are two Sigur Ros's this will error!
+Artist.objects.get(name="Sigur Ros")
+
+# Get all the Artists who are from Iceland
+Artist.objects.filter(nationality="Iceland")
+
+# Create an Artist with the following attributes, then save, commiting it to the database
+sigur_ros = Artist(name="Sigr Ros", photo_url="test.com", nationality="Iceland")
+sigur_ros.save()
+
+# Oops, we misspelled Sigur Ros's name! Let's change it and then commit to the DB
+sigur_ros.name = "Sigur Ros"
+sigur_ros.save()
+
+# Let's add a song to the artist
+song = Song(title="Hoppipolla", album="Takk", preview_url="test.com", artist=sigur_ros)
+song.save()
+
+# Delete the song
+song.delete()
+
+#Delete song that has a specific id
+Song.objects.get(id=1).delete()
+'''Above this line is new code'''
+
